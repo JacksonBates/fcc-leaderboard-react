f79cdd91b888f5862fa1ff4783e62a203694e97c
jacksonbates <malgalin@gmail.com>
2017-10-05 23:44:55 +1100

    adds fetch call to Table componentDidMount lifecycle

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,31 +1,58 @@
 import React from 'react';
 
-const Table = () => {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Leaderboard</th>
-          </tr>
-          <tr>
-            <th>#</th>
-            <th>Camper</th>
-            <th>30 Days</th>
-            <th>All Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>JacksonBates</td>
-            <td>123</td>
-            <td>456</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
+export default class Table extends React.Component {
+  state = {
+    recent: []
+  };
+  componentDidMount() {
+    const url = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        this.setState(() => ({
+          recent: data
+        }));
+      });
+  };
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Leaderboard</th>
+            </tr>
+            <tr>
+              <th>#</th>
+              <th>Camper</th>
+              <th>30 Days</th>
+              <th>All Time</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.recent.map((camper, index) => { 
+            return (
+              <tr key={camper.username}>
+                <td>{index + 1}</td>
+                <td>
+                  <img 
+                    src={camper.img} 
+                    alt='{username} profile picture' 
+                    height='40' 
+                    width='40'
+                  />
+                  {camper.username}
+                </td>
+                <td>{camper.recent}</td>
+                <td>{camper.alltime}</td>
+              </tr>
+            )}
+          )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
-
-export default Table;
