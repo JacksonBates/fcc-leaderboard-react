64235eff6401c44476c3f0548a518e5a85865e05
jacksonbates <malgalin@gmail.com>
2017-10-05 21:40:13 +1100

    hello worlds entry component

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LeaderboardApp from './components/LeaderboardApp';
+
+ReactDOM.render(<LeaderboardApp />, document.getElementById('app'));
